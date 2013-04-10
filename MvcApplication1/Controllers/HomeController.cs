f1c8e3b58bdbd5c8211dfcd7bd3993015240adc2
jing li <dnpxjean@hotmail.com>
2013-04-09 20:45:17 -0700

    aa

--- MvcApplication1/Controllers/HomeController.cs
+++ MvcApplication1/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace MvcApplication1.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "jean well done3";
+            ViewBag.Message = "jean well done1111111";
 
             return View();
         }
