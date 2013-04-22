985fad78f9ec777ceee62db38da9f1f8f9860dd9
unknown <v-jli@AP-jli-XA01.fareast.corp.microsoft.com>
2013-04-22 14:08:24 +0800

    third

--- MvcApplication1/Controllers/HomeController.cs
+++ MvcApplication1/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace MvcApplication1.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "jean well done!2";
+            ViewBag.Message = "jean well done!3";
 
             return View();
         }
